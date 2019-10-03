1d7d25b2ae62648589d91382d5185919a59adc45
sja16b <sja16b@my.fsu.edu>
2019-10-03 11:14:29 -0400

    Assignment 4 commit

---
+++ Assets/CountdownTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class CountdownTimer : MonoBehaviour
+{
+    public int timeLeft = 60; //Seconds Overall
+    public Text countdown; //UI Text Object
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+        countdown.text = ("test");
+    }
+    void Update()
+    {
+        countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+    }
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
