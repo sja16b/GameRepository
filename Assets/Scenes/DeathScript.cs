5f87285f41eb8b932bd21bbecaa5e35b5599e5fb
Alex Gilchrist <ang16m@my.fsu.edu>
2019-12-04 17:46:47 -0500

    DEATH SCRIPT

---
+++ Assets/Scenes/DeathScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour
+{
+	private int currentLvlIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentLvlIndex = SceneManager.GetActiveScene().buildIndex; 
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		SceneManager.LoadScene(currentLvlIndex);
+    }
+}
